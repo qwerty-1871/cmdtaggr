a37b8ad6dc8cc780470f86cb0635f0ce717e2a67
[qwerty] <60716753+qwerty-1871@users.noreply.github.com>
2024-05-23 00:52:12 -0500

    Add files via upload

--- Install.sh
+++ Install.sh
@@ -3,6 +3,7 @@
 sudo chmod +x cmdtaggr
 sudo chmod +x files/reset.sh
 mkdir files/tags
+mkdir files/tags/fl
 cp -r files ~/.cmdtaggr
 sudo cp cmdtaggr /usr/bin
 echo cmdtaggr has been installed successfully!
